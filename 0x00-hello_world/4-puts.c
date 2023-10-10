f6b193d29d8756b11673fbc4312e6d1409e20705
Philip Ajayi <philipajayi@outlook.com>
2023-10-10 18:06:51 +0100

    4 checked for errors and corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
+
 /**
-*main - this is the main put function
-*Return: 0
-*/
+ * main - this is the main put function
+ * Return: always 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
