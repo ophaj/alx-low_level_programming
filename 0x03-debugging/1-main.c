220bb2ce8d9958b0e544c5959657fc01b0d356cf
Philip Ajayi <philipajayi@outlook.com>
2023-10-15 22:02:30 +0100

    Like, comment and subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*while (i < 10)*/
@@ -15,5 +16,6 @@ int main(void)
 	/*putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
