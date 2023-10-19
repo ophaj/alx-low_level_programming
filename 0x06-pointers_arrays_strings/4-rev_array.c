1c6bc7f830bd7eeea154f64145c6354886054936
Philip Ajayi <philipajayi@outlook.com>
2023-10-19 01:56:59 +0100

    Pointers, arrays and strings tasks completed

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * reverse_array - function name
+  * @a: parameter one
+  * @n: parameter two
+  * Return: void
+  */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n / 2; i++)
+	{
+	j = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = j;
+	}
+
+}
