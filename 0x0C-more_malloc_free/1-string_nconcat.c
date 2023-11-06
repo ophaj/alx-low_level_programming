a361f96a4d4c2cb4ef19591ef9add4e016603d75
Philip Ajayi <philipajayi@outlook.com>
2023-11-07 00:13:56 +0100

    Task 1 Corrected.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,31 +13,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = n, index;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (s2 == NULL)
-		s2 = "";
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	if (!s)
 		return (NULL);
 
-	len = 0;
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	concat[len] = '\0';
+	s[i] = '\0';
 
-	return (concat);
+	return (s);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -10,4 +13,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
 #endif
