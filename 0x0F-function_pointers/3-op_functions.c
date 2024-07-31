3b70fdcd073bf3b016778f901b2213cfb102ceb5
ophaj <ajayi.philip@outlook.com>
2024-07-31 14:15:26 -0700

    Correction of 3-op_functions file.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,35 +1,64 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
+
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
 
 /**
-* get_op_func - This selects the correct function to perform
-*	the operation requested by the user.
-* @s: The operator passed as an argument.
-*
-* Return: A pointer to the function
-*	corresponding to the operator
-*	given as a parameter.
-*/
-int (*get_op_func(char *s))(int, int)
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}};
-
-	int i = 0;
-
-		while (i < 5)
-		{
-			if (strcmp(s, ops[i].op) == 0)
-				return (ops[i].f);
-
-		i++;
-	}
-
-	return (0);
+	return (a + b);
 }
+/**
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
